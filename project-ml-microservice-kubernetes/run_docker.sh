babe9760ed4e3c6140ab925ec277d5cf5abc2dff
Ubuntu <ubuntu@ip-172-31-30-249.ap-south-1.compute.internal>
2020-07-19 18:39:41 +0000

    completed project steps

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag annantguptacs15/udacity_predict . 
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it --rm -p 8000:80 --name predict_container annantguptacs15/udacity_predict 
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="annantguptacs15/udacity_predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl create deployment --image="$dockerpath" udacity-predict
+sleep 15
 # Step 3:
 # List kubernetes pods
+kubectl get pods 
 
 # Step 4:
 # Forward the container port to a host
-
+sudo kubectl port-forward --address 0.0.0.0  deployment/udacity-predict 8000:80
